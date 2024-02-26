ae3fad9da9cea57f37c5fd382b602b9491e6c5e0
whytf <lukasko.g@gmail.com>
2024-02-26 20:50:42 +0100

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -15,11 +15,11 @@ const Logo: Component<LogoProps> = originalProps => {
 		<A
 			href="/"
 			class={`flex flex-row gap-4 ${props.class ?? ''}`}
-			aria-label="metar.gg logo, links back to the home page">
+			aria-label="metar.sk logo, links back to the home page">
 			<LogoSvg class="w-12 flex-shrink-0 rounded-full bg-white text-primary transition-colors dark:bg-transparent dark:text-white-light" />
 			<Show when={props.showText}>
 				<span class="my-auto font-display text-2xl text-primary transition-colors dark:text-white-light">
-					metar.gg
+					metar.sk
 				</span>
 			</Show>
 		</A>
