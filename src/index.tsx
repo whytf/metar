f458da91fb84f98fe984f453731ef099833df3fb
whytf <lukasko.g@gmail.com>
2024-02-26 21:47:38 +0000

    Revert "contact tab update"
    This reverts commit b14c65bdc30aa35ddbfecec8aa4fa93e55f101c0.
    
    revert fix

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,6 @@ import Home from './pages/Home'
 import Legal from './pages/Legal'
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import TermsOfUse from './pages/TermsOfUse'
-import ContactUs from './pages/ContactUs'
 import './styles/index.css'
 
 const App: Component = () => {
@@ -33,7 +32,6 @@ const App: Component = () => {
 										<Route path="/legal" component={Legal} />
 										<Route path="/terms" component={TermsOfUse} />
 										<Route path="/privacy" component={PrivacyPolicy} />
-										<Route path="/contact" component={ContactUs} />
 										<Route path="/airport/:airportIdentifier" component={AirportSearchDetail} />
 										<Route
 											path="/:airportIdentifier"
