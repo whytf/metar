904a8526286c5cbcbe82162dfc769f6bb75aea5b
whytf <lukasko.g@gmail.com>
2024-02-26 21:51:33 +0100

    update privacy policy

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -12,19 +12,11 @@ const PrivacyPolicy = () => {
 				<Header />
 				<h1 class="pt-16 text-4xl font-bold">Privacy policy</h1>
 				<p class="mt-4">
-					We use Plausible Analytics (self-hosted) to track overall trends in the usage of our website.
-					Plausible Analytics collects only aggregated information, which does not allow us to identify any
-					visitor to our website. For more information, please visit the{' '}
-					<a href="https://plausible.io/data-policy" target="_blank">
-						https://plausible.io/data-policy
+					Original owner has implemented analytics, my fork is analytics free. For more information, please visit the{' '}
+					<a href="https://github.com/whytf/metar/blob/2952d8c604f2f1d55912b08562c8489453ea2497/index.html" target="_blank">
+						Commit removing analytics
 					</a>
 					.
-				</p>
-				<p class="mt-4">
-					The purpose of us using this software is to understand our website traffic in the most
-					privacy-friendly way possible so that we can continually improve our website. The lawful basis as
-					per the GDPR is "Article 6(1)(f); where our legitimate interests are to improve our website and
-					business continually." As per the explanation, no personal data is stored over time.
 				</p>
 			</div>
 		</PageContent>
