a9de1892246ef50db2c761cdadeb646506a9ee47
whytf <lukasko.g@gmail.com>
2024-02-26 21:54:48 +0100

    fix privacy policy link

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -13,7 +13,7 @@ const PrivacyPolicy = () => {
 				<h1 class="pt-16 text-4xl font-bold">Privacy policy</h1>
 				<p class="mt-4">
 					Original owner has implemented analytics, my fork is analytics free. For more information, please visit the{' '}
-					<a href="https://github.com/whytf/metar/blob/2952d8c604f2f1d55912b08562c8489453ea2497/index.html" target="_blank">
+					<a href="https://github.com/whytf/metar/commit/2952d8c604f2f1d55912b08562c8489453ea2497#diff-0eb547304658805aad788d320f10bf1f292797b5e6d745a3bf617584da017051" target="_blank">
 						Commit removing analytics
 					</a>
 					.
